31a24f66139a0224a85beee2cdba25ff656c71c2
fixztter <francoinformatica2017@gmail.com>
2023-05-18 13:07:24 -0300

    Setting up the proyect.

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/fixztter/rest-fundamentals/handlers"
 	"github.com/fixztter/rest-fundamentals/server"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	sv := server.NewServer(":3000")
-	err := sv.ListenAndServe()
-	if err != nil {
-		panic(err)
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file")
 	}
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+	s, err := server.NewServer(context.Background(), &server.Config{
+		Port:        PORT,
+		JWTSecret:   JWT_SECRET,
+		DatabaseUrl: DATABASE_URL,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
 }
--- server/server.go
+++ server/server.go
@@ -1,21 +1,55 @@
 package server
 
 import (
+	"context"
+	"errors"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type User struct {
-	Id       int
-	Nickname string
+type Config struct {
+	Port        string
+	JWTSecret   string
+	DatabaseUrl string
 }
 
-var (
-	usrs []*User
-)
+type Server interface {
+	Config() *Config
+}
 
-func NewServer(addr string) *http.Server {
-	InitRoutes()
-	return &http.Server{
-		Addr: addr,
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+func (b *Broker) Config() *Config {
+	return b.config
+}
+
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
+	if config.Port == "" {
+		return nil, errors.New("Port is required")
+	}
+	if config.JWTSecret == "" {
+		return nil, errors.New("Secret is required")
+	}
+	if config.DatabaseUrl == "" {
+		return nil, errors.New("Database URL is required")
+	}
+	broker := &Broker{
+		config: config,
+		router: mux.NewRouter(),
+	}
+	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.router = mux.NewRouter()
+	binder(b, b.router)
+	log.Printf("Starting server on port%s\n", b.Config().Port)
+	if err := http.ListenAndServe(b.Config().Port, b.router); err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
